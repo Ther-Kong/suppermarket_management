9bed2b726e950c2ad52d142d66f9ef051361855c
Ther Kong <khcsgm@gmail.com>
2021-05-28 16:34:12 +0800

    create project

---
+++ src/main/java/com/example/supermarket_management/SupermarketManagementApplication.java
@@ -0,0 +1,13 @@
+package com.example.supermarket_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupermarketManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SupermarketManagementApplication.class, args);
+    }
+
+}
