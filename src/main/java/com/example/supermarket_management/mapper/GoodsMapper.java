87c749a0bce1281a90ef1e6bed5a500358fb9fbb
lmilanlsz <lmilanlsz@outlook.com>
2021-06-15 21:46:22 +0800

    lsz unitTest Trash

--- src/main/java/com/example/supermarket_management/mapper/GoodsMapper.java
+++ src/main/java/com/example/supermarket_management/mapper/GoodsMapper.java
@@ -2,12 +2,25 @@ package com.example.supermarket_management.mapper;
 
 import com.example.supermarket_management.pojo.Goods;
 import org.apache.ibatis.annotations.*;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 
 @Mapper
 @Repository
+//@SpringBootApplication
+//@RestController
+//@ComponentScan(basePackages={"**.***.**"})
+//@ServletComponentScan(basePackages={"**.***.**"})
+//@MapperScan(basePackages={"**.***.**"}, sqlSessionFactoryRef = "sqlSessionFactory")
+//@EnableScheduling //定时任务注解
 public interface GoodsMapper {
     @Select("select * from goods where status = 0")
     @Results({
